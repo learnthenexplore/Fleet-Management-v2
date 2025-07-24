f68847699d50f1c1ec6b503b3fdfa80ccc7963b2
learnthenexplore <learnthenexplore@gmail.com>
2025-07-24 14:45:35 +0400

    u

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 
-export const BASE_URL = 'http://localhost:5000';
+// export const BASE_URL = 'http://localhost:5000';
 // or for production:
 // export const BASE_URL = 'https://fleet-management-bn9l.onrender.com';
-// export const BASE_URL = 'https://fleet-management-v2.onrender.com';
+export const BASE_URL = 'https://fleet-management-v2.onrender.com';
